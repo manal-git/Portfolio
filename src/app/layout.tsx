393f8299f0cb74098a93d94845606afd49555239
manal-git <khalqallahmanal@gmail.com>
2026-07-01 16:46:13 +0200

    font retro2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
 
+const retro = Press_Start_2P({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-retro",
+});
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-black antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${retro.variable} bg-black antialiased`}
       >
         {children}
       </body>
